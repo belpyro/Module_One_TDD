d8b6ae66d9c70c2bab41b820448ca9e5d0a1dc3d
Alice <LisaAlisaLis888>
2020-04-16 20:36:12 +0300

    Added calculator methods + tests

---
+++ CalculatorSample/CalculatorSample.Logic/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+
+namespace CalculatorSample.Logic
+{
+    public class Calculator
+    {
+        private ILogger _logger;
+
+        public Calculator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public int Sum(int x, int y)
+        {
+            _logger.Log($"Operation Sum: x = {x}, y = {y}");
+            return x + y;
+        }
+
+        public double Exponentiations(double x, double y)
+        {
+            _logger.Log($"Operation Sum: x = {x}, y = {y}");
+            return Math.Pow(x, y);
+        }
+    }
+
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            File.AppendAllText("log.txt", message);
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}

37001bcfb78216203360246337a09f32228ec8d1
Alexander Shaduro <ashad@sam-solutions.net>
2020-04-17 17:54:36 +0300

    First lecture sample

---
+++ CalculatorSample/CalculatorSample.Logic/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace CalculatorSample.Logic
+{
+    public class Calculator
+    {
+        private ILogger _logger;
+
+        public Calculator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public int Add(int x, int y)
+        {
+            _logger.Log($"Operation Sum: x={x}, y={y}");
+            return x + y;
+        }
+    }
+
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            File.AppendAllText("log.txt", message);
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
---
+++ CalculatorSample/CalculatorSample.Tests/CalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using CalculatorSample.Logic;
+using Moq;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace CalculatorSample.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Test_Sum_Positive_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Add(3, 4);
+            mock.Verify(l => l.Log(It.IsAny<string>()), Times.Exactly(1));
+            Assert.AreEqual(expected: 7, actual);
+        }
+
+        [Test]
+        public void Test_Sum_Negative_Numbers()
+        {
+            var mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+
+            var calc = new Calculator(mock.Object);
+            var actual = calc.Add(-5, -8);
+            Assert.AreEqual(expected: -13, actual);
+        }
+    }
+}

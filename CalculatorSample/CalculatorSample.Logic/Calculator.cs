1cfbd369aa712ea48017d95c52d2c9951e22361f
franken <frank_60@mail.ru>
2020-05-17 13:37:09 +0300

    Выполнено Задание 1:
    -завершить разработку калькулятора для основных арифметических операций: сложение, вычитание, деление, умножение, возведение в степень
    -покрыть класс калькулятора модульными тестами
    -добавить логирование в операции калькулятора

--- CalculatorSample/CalculatorSample.Logic/Calculator.cs
+++ CalculatorSample/CalculatorSample.Logic/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace CalculatorSample.Logic
 {
@@ -11,11 +12,72 @@ namespace CalculatorSample.Logic
             _logger = logger;
         }
 
-        public int Add(int x, int y)
+        /// <summary>
+        /// Операция сложения
+        /// </summary>
+        /// <param name="x">Принимает дробное число</param>
+        /// <param name="y">Принимает дробное число</param>
+        /// <returns>Возвращает результат: x + y</returns>
+        public double Add(double x, double y)
         {
             _logger.Log($"Operation Sum: x={x}, y={y}");
             return x + y;
         }
+
+        /// <summary>
+        /// Операция вычитания
+        /// </summary>
+        /// <param name="x">Принимает дробное число</param>
+        /// <param name="y">Принимает дробное число</param>
+        /// <returns>Возвращает результат: x - y</returns>
+        public int Subtract(int x, int y)
+        {
+            _logger.Log($"Operation Subtract: x={x}, y={y}");
+            return x - y;
+        }
+
+
+        /// <summary>
+        /// Операция деления
+        /// </summary>
+        /// <param name="x">Принимает дробное число</param>
+        /// <param name="y">Принимает дробное число</param>
+        /// <returns>Возвращает результат: x / y</returns>
+        public double Divide(double x, double y)
+        {
+            _logger.Log($"Operation Divide: x={x}, y={y}");
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+
+        /// <summary>
+        /// Операция умножения
+        /// </summary>
+        /// <param name="x">Принимает дробное число</param>
+        /// <param name="y">Принимает дробное число</param>
+        /// <returns>Возвращает результат: x / y</returns>
+        public double Multiply(double x, double y)
+        {
+            _logger.Log($"Operation Multiply: x={x}, y={y}");
+            return x * y;
+        }
+
+
+        /// <summary>
+        /// Операция возведение в степень
+        /// </summary>
+        /// <param name="x">Принимает дробное число</param>
+        /// <param name="y">Принимает дробное число</param>
+        /// <returns>Возвращает результат: x^y</returns>
+        public double Exponentiation(double x, double y)
+        {
+            _logger.Log($"Operation Exponentiation: x={x}, y={y}");
+            return Math.Pow(x,y);
+        }
+
     }
 
     public class Logger : ILogger
--- CalculatorSample/CalculatorSample.Tests/CalculatorTest.cs
+++ CalculatorSample/CalculatorSample.Tests/CalculatorTest.cs
@@ -8,27 +8,82 @@ namespace CalculatorSample.Tests
     [TestFixture]
     public class CalculatorTest
     {
+        private Mock<ILogger> mock;
+        private Calculator calc;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            mock = new Mock<ILogger>();
+            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
+            calc = new Calculator(mock.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mock.Verify(l => l.Log(It.IsAny<string>()), Times.Exactly(1));
+            mock = null;
+            calc = null;
+        }
+
         [Test]
         public void Test_Sum_Positive_Numbers()
         {
-            var mock = new Mock<ILogger>();
-            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
-
-            var calc = new Calculator(mock.Object);
             var actual = calc.Add(3, 4);
             mock.Verify(l => l.Log(It.IsAny<string>()), Times.Exactly(1));
-            Assert.AreEqual(expected: 7, actual);
+            Assert.AreEqual(expected: 7, actual: actual);
         }
 
         [Test]
         public void Test_Sum_Negative_Numbers()
         {
-            var mock = new Mock<ILogger>();
-            mock.Setup(l => l.Log(It.IsAny<string>())).Callback<string>(s => Debug.WriteLine(s));
-
-            var calc = new Calculator(mock.Object);
             var actual = calc.Add(-5, -8);
-            Assert.AreEqual(expected: -13, actual);
+            Assert.AreEqual(expected: -13, actual: actual);
+        }
+
+
+        [TestCase(3,4,-1)]
+        [TestCase(5,4,1)]
+        [TestCase(-9,-3,-6)]
+        public void Calculator_Subtract_ReturnCorrectResult(int x,int y,int expected)
+        {
+            var actual = calc.Subtract(x, y);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 4, 0.25d)]
+        [TestCase(10, 2, 5)]
+        [TestCase(-9, -3, 3)]
+        public void Calculator_Divide_ReturnCorrectResult(double x, double y, double expected)
+        {
+            var actual = calc.Divide(x, y);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Calculator_Divide_CatchDivideByZeroException()
+        {
+            Assert.Catch<System.DivideByZeroException>(()=>calc.Divide(-4,0));
+        }
+
+        [TestCase(4.5, 4.5, 20.25d)]
+        [TestCase(10, 2, 20)]
+        [TestCase(-3, 3, -9)]
+        public void Calculator_Multiply_ReturnCorrectResult(double x, double y, double expected)
+        {
+            var actual = calc.Multiply(x, y);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+
+        [TestCase(3, 2, 9)]
+        [TestCase(5, 3, 125)]
+        [TestCase(-7, 7, -823543)]
+        public void Calculator_Exponentiation_ReturnCorrectResult(double x, double y, double expected)
+        {
+            var actual = calc.Exponentiation(x, y);
+            Assert.AreEqual(expected: expected, actual: actual);
         }
     }
 }
